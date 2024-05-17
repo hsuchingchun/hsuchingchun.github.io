4e90b9490039d796627629025a23554b490a63ef
hsuchingchun <48717305+hsuchingchun@users.noreply.github.com>
2024-05-17 13:33:01 +0800

    nccuspace_project

--- project/project.js
+++ project/project.js
@@ -14,3 +14,25 @@
 //       wrapper.appendChild(iframe);
 //     }
 //   });
+
+// window.onload = function () {
+//   const images = document.querySelectorAll("img");
+//   images.forEach((img) => {
+//     const wrapper = document.createElement("div");
+//     wrapper.classList.add("img-container");
+//     img.parentElement.insertBefore(wrapper, img);
+//     wrapper.appendChild(img);
+
+//     wrapper.addEventListener("click", function () {
+//       const isZoomed = img.style.transform === "scale(2)";
+//       img.style.transform = isZoomed ? "scale(1)" : "scale(2)";
+//       const body = document.querySelector("body");
+//       body.style.backdropFilter = isZoomed ? "blur(0)" : "blur(5px)";
+//       img.style.zIndex = isZoomed ? "0" : "1"; // 設置 z-index
+//     });
+
+//     wrapper.addEventListener("mouseleave", function () {
+//       img.style.transform = "scale(1)";
+//     });
+//   });
+// };
